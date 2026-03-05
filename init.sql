be3193e75e4f0d55957d3801a618294f66c2e154
Elias Rinne <elias1rinne@gmail.com>
2026-03-05 18:07:46 +0200

    fix docker db isssues

--- init.sql
+++ init.sql
@@ -2,9 +2,7 @@ CREATE DATABASE IF NOT EXISTS calc_data;
 USE calc_data;
 
 CREATE USER IF NOT EXISTS 'app_user'@'%' IDENTIFIED BY 'STRONG_PASSWORD';
-
 GRANT ALL PRIVILEGES ON calc_data.* TO 'app_user'@'%';
-
 FLUSH PRIVILEGES;
 
 CREATE TABLE IF NOT EXISTS calc_results (
@@ -13,5 +11,13 @@ CREATE TABLE IF NOT EXISTS calc_results (
                                             number2 DOUBLE NOT NULL,
                                             sum_result DOUBLE NOT NULL,
                                             product_result DOUBLE NOT NULL,
+                                            subtraction_result DOUBLE NOT NULL,
+                                            division_result DOUBLE NULL,
                                             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Example data so Jenkins / screenshots show results
+INSERT INTO calc_results
+(number1, number2, sum_result, product_result, subtraction_result, division_result)
+VALUES
+    (2, 22, 24, 44, -20, 0.09090909090909091);
